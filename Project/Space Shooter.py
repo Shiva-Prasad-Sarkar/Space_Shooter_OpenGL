c5391d588e158532d6a2c6c357bbc683d5466dac
SHIVA PRASAD SARKAR <shiva.prasad.sarkar@g.bracu.ac.bd>
2025-05-05 23:39:19 +0600

    v003
    first person view, stars added, fixed ratio

--- Project/Space Shooter.py
+++ Project/Space Shooter.py
@@ -10,27 +10,24 @@ x_r,y_r,z_r = False,False,False
 start_game = False
 wlc = True
 over = False
-angle  = 0
-gr_len = 90
 fpview = False
 scaling  = True
 
 #variables
 time_1 = time.time()
-c_x, c_y, c_z = 0,5, 500  # Elevated and pulled back
-camera_pos = (c_x, c_y, c_z)
-fovY = 120  # Field of view for perspective
-p_x,p_y=0,0
+camera_pos = (0,5, 500 )
+fovY = 120  
+p_x,p_y= 0, 0
+angle  = 0
+gr_len = 90
 
-def draw_chess_borad():
+def space():
     global gr_len
     for r in range(12):
         begin_y =gr_len*6 - r*gr_len
         for j in range(12):
             begin_x = gr_len*6 - j*gr_len
             glBegin(GL_QUADS)
-            #selecting alternate color
-
             glColor3f(0.4, 0.4, 0.95)
             glVertex3f(begin_x,begin_y,0)
             glVertex3f(begin_x - gr_len, begin_y, 0)
@@ -38,7 +35,18 @@ def draw_chess_borad():
             glVertex3f(begin_x, begin_y - gr_len, 0)
             glEnd()
 
-def game_wall():
+
+def draw_star():
+    for x in range(-500, 501, 100): 
+        for y in range(-500, 501, 100): 
+            glPointSize(2.5)
+            glBegin(GL_POINTS)
+            glColor3f(1.0, 1.0, 1.0)
+            glVertex3f(x + 20, y + 40, 2) 
+            glEnd()
+
+
+def space_boarder():
     global gr_len
     ht = 7
 
@@ -74,7 +82,7 @@ def draw_text(x, y, text, font=GLUT_BITMAP_HELVETICA_18):
     glMatrixMode(GL_PROJECTION)
     glPushMatrix()
     glLoadIdentity()
-    gluOrtho2D(0, 1280, 0, 720)  # Updated for new window size
+    gluOrtho2D(0, 1280, 0, 720) 
     glMatrixMode(GL_MODELVIEW)
     glPushMatrix()
     glLoadIdentity()
@@ -89,7 +97,6 @@ def draw_text(x, y, text, font=GLUT_BITMAP_HELVETICA_18):
     glMatrixMode(GL_MODELVIEW)
 
 def game_begin():
-    
     glMatrixMode(GL_PROJECTION)
     glPushMatrix()
     glLoadIdentity()
@@ -117,12 +124,10 @@ def game_begin():
     glEnd()
 
     
-    draw_text(box_x + 100, box_y + 25, "Start Game", GLUT_BITMAP_TIMES_ROMAN_24)
-    draw_text(box_x + 30, box_y - 25, "Welcome to Space Shooter!", GLUT_BITMAP_HELVETICA_18)
-    draw_text(box_x + 100, box_y - 50, "Press 'g' to Start", GLUT_BITMAP_HELVETICA_12)
+    draw_text(box_x + 80, box_y + 25, "Start Game", GLUT_BITMAP_TIMES_ROMAN_24)
+    draw_text(box_x , box_y - 25, "Welcome to Space Shooter!", GLUT_BITMAP_HELVETICA_18)
+    draw_text(box_x + 85, box_y - 50, "Press 'g' to Start", GLUT_BITMAP_HELVETICA_12)
 
-    
-    # Restore previous projection/modelview states
     glPopMatrix()
     glMatrixMode(GL_PROJECTION)
     glPopMatrix()
@@ -133,14 +138,13 @@ def fighter_spaceship():
     global over,p_x,p_y,angle,x_r,y_r,z_r,scaling
 
     glPushMatrix()
-    glTranslatef(p_x,p_y,5)
+    glTranslatef(p_x,p_y,30)
     glRotatef(-90, 1, 0, 0)
 
     if scaling:
-        glScalef (1.05, 1.05, 1) 
+        glScalef (.9, .9, 1) 
     else:
-        glScalef (.95, .95, 1) 
-
+        glScalef (.8, .8, 1) 
 
     if x_r:
         glRotatef(angle, 1, 0, 0)
@@ -150,38 +154,30 @@ def fighter_spaceship():
         glRotatef(angle, 0, 0, 1)
 
 
-    glColor3f(0, 0, .8)
     glPushMatrix()
-    glScalef(1.5, .5, 1) 
+    glColor3f(1, .4, .4)
+    glScalef(1.5, .6, 1) 
     glutSolidCube(60)
     glPopMatrix()
+    
+    glPushMatrix()
+    glColor3f(.7, .8, .8)
+    glTranslatef(0, 0, -90)  
+    gluCylinder(gluNewQuadric(), 3, 20, 60, 10, 10)
+    glPopMatrix()
 
     glPushMatrix()
-    glColor3f(0, 1, 0)
+    glColor3f(2, 1, 0)
     glTranslatef(-55, 0, 0)
-    gluCylinder(gluNewQuadric(), 13, 9, 60, 10, 10)
+    gluCylinder(gluNewQuadric(), 10, 15, 60, 10, 10)
     glPopMatrix()
 
-    
     glPushMatrix()
-    glColor3f(0, 1, 0)
+    glColor3f(1, 1, 0)
     glTranslatef(55, 0, 0)
-    gluCylinder(gluNewQuadric(), 13, 9, 60, 10, 10)
+    gluCylinder(gluNewQuadric(), 10, 15, 60, 10, 10)
     glPopMatrix()
-
-
-    glPushMatrix()
-    glColor3f(0, 1, 1)
-    glTranslatef(0, 0, -40)  
-    gluCylinder(gluNewQuadric(), 5, 25, 100, 10, 10)
-    glPopMatrix()
-
     
-    glPushMatrix()
-    glColor3f(0, 1, 1)
-    glTranslatef(0, 0, 40) 
-    gluCylinder(gluNewQuadric(), 5, 5, 20, 10, 10)
-    glPopMatrix()
 
     glPopMatrix()
 
@@ -225,15 +221,12 @@ def keyboardListener(key, x, y):
         wlc = False
         start_game = True
 
-    elif key == b'v':
+    elif key == b'f':
         global fpview
         fpview = not fpview
 
 
 def specialKeyListener(key, x, y):
-    """
-    Handles special key inputs (arrow keys) for adjusting the camera angle and height.
-    """
     global camera_pos
     x, y, z = camera_pos
     if key == GLUT_KEY_LEFT:
@@ -249,40 +242,39 @@ def specialKeyListener(key, x, y):
         if y>5:
             y -= 5
     camera_pos = (x, y, z)
-    print(camera_pos)
+    
 
 def mouseListener(button, state, x, y):
-    """
-    Handles mouse inputs for firing bullets (left click) and toggling camera mode (right click).
-    """
     pass  # To be implemented
 
+
 def setupCamera():
-    global fpview
-    glMatrixMode(GL_PROJECTION)  
-    glLoadIdentity()  
-    gluPerspective(fovY, 1.25, 0.1, 1500) 
-    glMatrixMode(GL_MODELVIEW) 
-    glLoadIdentity() 
     global fpview, p_x, p_y, angle
 
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    gluPerspective(fovY, 1.25, 0.1, 1500)
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
+
     if fpview:
-        n_x = p_x - math.cos(math.radians(angle)) * 25
-        n_y = p_y - math.sin(math.radians(angle)) * 25
-        n_z = 40
-        x = p_x - math.cos(math.radians(angle)) * 90
-        y = p_y - math.sin(math.radians(angle)) * 90
-        z = 35
+        n_x = p_x
+        n_y = p_y - 40   # behind
+        n_z = 50        # above
+        x = p_x
+        y = p_y + 90
+        z = 50         # downward a bit
+
         gluLookAt(n_x, n_y, n_z, 
-                  x, y, z, 
-                  0, 0, 1)
+                x, -y, z, 
+                0, 0, 1)
         
+    
     else:
         x, y, z = camera_pos
-        gluLookAt(x, y, z,  # Cam pos
-                0, 0, 0,  # Look tar
-                0, 0, 1)  # Upp vector (z_axis)
-
+        gluLookAt(x, y, z, 
+                  0, 0, 0, 
+                  0, 0, 1)
 
 
 def idle():
@@ -301,38 +293,32 @@ def showScreen():
     glLoadIdentity()
     glViewport(0, 0, 1000, 800)
     setupCamera()
-
    
     if wlc:
         game_begin()
         
-        
     else:
+        space()
+        space_boarder()
+        draw_star()
         fighter_spaceship()
-        draw_chess_borad()
-        game_wall()
        
-
-
     glutSwapBuffers()
 
 def main():
     glutInit()
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH)
-    glutInitWindowSize(1000, 800)  # Updated window size
+    glutInitWindowSize(1000, 800) 
     glutInitWindowPosition(0, 0)
     glutCreateWindow(b"3D Space Shooter")
-
-    glEnable(GL_DEPTH_TEST)  # Enable depth for 3D rendering
-
+     
     glutDisplayFunc(showScreen)
     glutKeyboardFunc(keyboardListener)
     glutSpecialFunc(specialKeyListener)
     glutMouseFunc(mouseListener)
     glutIdleFunc(idle)
-    
-
     glutMainLoop()
+    
 
 if __name__ == "__main__":
     main()
